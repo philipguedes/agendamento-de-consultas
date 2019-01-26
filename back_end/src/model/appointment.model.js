f2dcb2351248e9fe6526f5a3bb25647ef431f1d3
philipguedes <philipguedes@gmail.com>
2019-01-26 20:38:07 -0200

    Modifica appointment.model

--- back_end/src/model/appointment.model.js
+++ back_end/src/model/appointment.model.js
@@ -6,15 +6,16 @@ const AppointmentSchema = new Schema({
   status: { type: String },
   created: { type: Date, default: Date.now },
   schedule: { type: Date, required: 'This date is required!', unique: true, index: true },
-  updates: [{
-    created: { type: Date, default: Date.now },
-    reason: { type: String }
-  }],
+  // updates: [{
+  //   created: { type: Date, default: Date.now },
+  //   reason: { type: String }
+  // }],
   user: {
     email: { type: String, required: 'Email is required!' },
     phone: { type: String, required: 'Phone is required!' },
     name: { type: String, required: 'Name is required!' }
-  }
+  },
+  free: { type: Boolean }
 })
 
 module.exports = model('Appointment', AppointmentSchema)
