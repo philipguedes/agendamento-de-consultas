f0ca5da293322c2b4f9dfd7edf0db0fe992f2bc5
philipguedes <philipguedes@gmail.com>
2019-01-28 23:05:38 -0200

    Dockerfile da api + rota de health

--- back_end/src/routes/index.js
+++ back_end/src/routes/index.js
@@ -5,4 +5,5 @@ const agendaRoute = require('./agenda')
 module.exports = (app) => {
   app.use('/appointments', appointmentsRoute)
   app.use('/agenda', agendaRoute)
+  app.use('/health', (req, res) => { res.status(200).send('ok') })
 }
