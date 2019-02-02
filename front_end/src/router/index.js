3e560db7093ef04aedd062f1fec2fba239253a32
philipguedes <philipguedes@gmail.com>
2019-02-02 00:34:05 -0200

    Corrige erro de lint

--- front_end/src/router/index.js
+++ front_end/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Scheduling from '@/views/appointment/Scheduling'
 import Appointment from '@/pages/Appointment'
 import Admin from '@/pages/Admin'
 
